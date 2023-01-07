2012440cc7dbbec12de7c46aa0a2a48fd7ae912f
carbonium14 <1075413520@qq.com>
2023-01-07 11:11:07 +0800

    add error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { AuthenticatedApp } from 'authenticated-app';
+import { ErrorBoundary } from 'components/error-boundary';
+import { FullPageErrorFallBack } from 'components/lib';
 import { useAuth } from 'context/auth-context';
 import { UnAuthenticatedApp } from 'unauthenticated-app';
 import './App.css';
@@ -6,9 +8,9 @@ function App() {
   const {user} = useAuth()
   return (
     <div>
-    {
-      user?<AuthenticatedApp></AuthenticatedApp>:<UnAuthenticatedApp></UnAuthenticatedApp>
-    }
+      <ErrorBoundary fallbackRender={FullPageErrorFallBack}>
+        {user?<AuthenticatedApp></AuthenticatedApp>:<UnAuthenticatedApp></UnAuthenticatedApp>}
+      </ErrorBoundary>
     </div>
   );
 }
